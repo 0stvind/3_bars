99b9a4a06df72c43c02a2004d5fff22eb89f8bc6
0stvind <zakhar.kor@gmail.com>
2016-10-07 23:17:24 +0400

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,46 +1,54 @@
 import json
 
+
 def load_data(filepath):
     with open(filepath, 'r') as file_handler:
         bars = json.load(file_handler)
-    bars_info = dict()
-    for bar_number,bar in enumerate(bars):      
-        bars_info[bars[bar_number]['Cells']['Name']] = [bars[bar_number]['Cells']['geoData']['coordinates'] , bars[bar_number]['Cells']['SeatsCount']]
+    bars_info = []
+    for bar_number, bar in enumerate(bars):
+        temp = {'Name': bars[bar_number]['Cells']['Name'],
+                'Seats': bars[bar_number]['Cells']['SeatsCount'],
+                'coordinates':
+                bars[bar_number]['Cells']['geoData']['coordinates']}
+        bars_info.append(temp)
     return bars_info
+
 
 def get_biggest_bar(data):
     bars = data
-    max_number = 0
-    biggest = ''
-    for bar_name in bars:
-      if(bars[bar_name][1] > max_number):
-        biggest = bar_name
-        max_number = bars[bar_name][1]
-    return biggest
+    biggest = max(bars, key=lambda bar: bar['Seats'])
+    return biggest['Name']
 
 
 def get_smallest_bar(data):
     bars = data
-    min_number = 1000000
-    smallest = ''
-    for bar_name in bars:    
-      if(bars[bar_name][1] < min_number):
-        smallest = bar_name
-    return smallest
+    smallest = min(bars, key=lambda bar: bar['Seats'])
+    return smallest['Name']
 
 
 def get_closest_bar(data, longitude, latitude):
     bars = data
-    min_difference = 1000000
+    min_diff = 1000000
     closest = ''
-    for bar_name in bars:
-      if(abs((longitude + latitude) - bars[bar_name][0][0] + bars[bar_name][0][1]) < min_difference):
-        closest = bar_name
-        min_difference = abs((longitude + latitude) - bars[bar_name][0][0] + bars[bar_name][0][1])
+    for bar_number, bar in enumerate(bars):
+        if(abs((longitude + latitude) - bars[bar_number]['coordinates'][0] +
+               bars[bar_number]['coordinates'][1]) < min_diff):
+            closest = bars[bar_number]['Name']
+            longitude1 = bars[bar_number]['coordinates'][0]
+            latitude1 = bars[bar_number]['coordinates'][1]
+            bars[bar_number]['coordinates'][1]
+            min_diff = abs(longitude - longitude1) + abs(latitude - latitude1)
     return closest
 if __name__ == '__main__':
-    load_data('bars.json')
-    biggest = get_biggest_bar(load_data('bars.json'))
-    smallest = get_smallest_bar(load_data('bars.json'))
-    closest = get_closest_bar(load_data('bars.json'), 35, 48)
-    print('The biggest bar is %s. The smallest bar is %s. The closest bar is %s' % (biggest, smallest, closest))
+    print('Введите путь до файла ')
+    filepath = input()
+    print('Введите вашу долготу')
+    longitude = float(input())
+    print('Введите вашу широту')
+    latitude = float(input())
+    load_data(filepath)
+    biggest = get_biggest_bar(load_data(filepath))
+    smallest = get_smallest_bar(load_data(filepath))
+    closest = get_closest_bar(load_data(filepath), longitude, latitude)
+print("The biggest bar is %s. The smallest bar is %s."
+      "The closest bar is %s" % (biggest, smallest, closest))
